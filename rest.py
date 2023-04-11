f313194c186fbc8ae66ab22b1c9011d6641d40e9
Chelosky-O <marcelofernando.mv@gmail.com>
2023-04-10 22:02:51 -0400

    add time

--- rest.py
+++ rest.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 #Obtiene el id y nombre de los tragos del 11000 -> 16000
 def get_by_id():
@@ -7,7 +8,9 @@ def get_by_id():
     for i in range(11000,16000):
         url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={}".format(i)
         #url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000"
+        start = time.time()
         response = requests.get(url)
+        end = time.time()
         data = response.json()
         jsonified_data = json.dumps(data)
         
@@ -19,6 +22,7 @@ def get_by_id():
         if data['drinks'] != None:
             nombre = data['drinks'][0]['strDrink']
             print(str(id) + " : " + nombre)
+            print((end - start))
 
         #else:
          #   print(str(id) + " : " + "ERROR")
