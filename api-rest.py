4ff235a6f8cf50c31230fb9f9c88ab50c639a148
Failedvixo <101746155+Failedvixo@users.noreply.github.com>
2023-04-12 20:26:41 -0400

    Update api-rest.py

--- api-rest.py
+++ api-rest.py
@@ -4,43 +4,50 @@ import time
 import random
 import redis
 
+def getfromDocker(id, puerto):
+    r=redis.Redis(host='localhost', port=puerto)
+    cocktail=r.get(id)
+    if(cocktail== None):
+        searchBackend(id, puerto)
+    else:
+         print(str(id) + " : " + str(cocktail))
+        
+def searchBackend(id, puerto):
+    r=redis.Redis(host='localhost', port=puerto)
+    url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={}".format(id)
+    #url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000"
+    response = requests.get(url)
+    data = response.json()
+    jsonified_data = json.dumps(data)
+            
+            
+    if data['drinks'] != None:
+        nombre = data['drinks'][0]['strDrink']
+        print(str(id) + " : " + nombre)
+        r.set(id, data['drinks'][0]['strDrink'])
+    else:
+                print(str(id) + " : " + "ERROR")
+
 #Obtiene el id y nombre de los tragos del 11000 -> 16000
 def get_by_id():
     
-    n = input("Ingrese numero de requests: ")
+    n = int(input("Ingrese numero de requests: "))
     i = 0
-    r1=redis.Redis(host='localhost', port=6379)
-    
     file = open('data.txt','w')
-    
-    while(i!=n):
+    while(i<=n):
+        print("Iteración: ", str(i))
         id = random.randint(11000,16000);
         inicio=time.time()
-        dato=r1.get(id)
-        if(dato== None):
-            url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={}".format(id)
-            #url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000"
-            response = requests.get(url)
-            data = response.json()
-            jsonified_data = json.dumps(data)
-            
-            print("Iteración: ", str(i))
-            
-            if data['drinks'] != None:
-                nombre = data['drinks'][0]['strDrink']
-                print(str(id) + " : " + nombre)
-                r1.set(id, data['drinks'][0]['strDrink'])
-
-            else:
-                print(str(id) + " : " + "ERROR")
+        if (id>=11000 and id<12000):
+            getfromDocker(id, 6379)
+        elif (id>=12000 and id<14000):
+            getfromDocker(id, 6380)
         else:
-            print(str(id) + " : " + str(dato))
+             getfromDocker(id, 6381)
         final=time.time()
         print(final-inicio)
-        
-        file.write(str((final-inicio))+'\n')
-        
         i=i+1
+        file.write(str((final-inicio))+'\n')
         
 
 
